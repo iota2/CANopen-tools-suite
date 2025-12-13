c1fdcd05d7dc85ded251a82bbb6cc7ce4cc656d7
Naval Prashar <naval.prashar@gmail.com>
2025-12-14 02:47:36 +0530

    updated TUI table widths

--- canopen_analyzer/dox/documentation/html/canopen__sniffer_8py.js
+++
@@ -1,7 +0,0 @@
-var canopen__sniffer_8py =
-[
-    [ "can_sniffer", "classcanopen__sniffer_1_1can__sniffer.html", "classcanopen__sniffer_1_1can__sniffer" ],
-    [ "eds_parser", "classcanopen__sniffer_1_1eds__parser.html", "classcanopen__sniffer_1_1eds__parser" ],
-    [ "process_frame", "classcanopen__sniffer_1_1process__frame.html", "classcanopen__sniffer_1_1process__frame" ],
-    [ "main", "canopen__sniffer_8py.html#a355f06d5ba7ea264dc3e2e128349c130", null ]
-];
